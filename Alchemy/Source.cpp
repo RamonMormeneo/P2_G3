03018c6db08a139f5f25c669bababc9debf9fa74
Dolors <Dolors@DESKTOP-PC>
2017-04-25 18:41:03 +0200

    ufffff

--- Alchemy/Source.cpp
+++ Alchemy/Source.cpp
@@ -9,14 +9,18 @@ struct Key
 };
 void main()
 {
-
+	std::unordered_map<std::pair<std::string,std::string>, std::string, Key> fusions;
 	std::vector<std::string> Linea_Separada;
-	std::ifstream ftoread();
-	std::string Linea="Oceanix = Agua + Infancia";
-	Key key;
-	std::size_t posdeligual = Linea.find("=");
-	std::size_t posdelmas = Linea.find("+");
-	std::string result = Linea.substr(0, posdeligual - 1);
-	key.key1 = Linea.substr(posdeligual + 2, posdelmas);
-	key.key2 = Linea.substr(posdelmas+2);
+	std::ifstream ftoread("elements.dat");
+	std::string Linea;
+	while (std::getline(ftoread, Linea))
+	{
+		Key key;
+		std::size_t posdeligual = Linea.find("=");
+		std::string result = Linea.substr(0, posdeligual - 1);
+		Linea = Linea.substr(posdeligual + 2);
+		std::size_t posdelmas = Linea.find("+");
+		key.key1 = Linea.substr(0, posdelmas - 1);
+		key.key2 = Linea.substr(posdelmas + 2);
+	}
 }
