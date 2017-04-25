7cf4759a5c65feda63525c3e8943416a91debe23
ramonmormeneoferrer <ramonmormeneoferrer@S1-08>
2017-04-25 12:31:39 +0200

    Add project files.

---
+++ Alchemy/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <unordered_map>
+#include <fstream>
+#include <vector>
+#include <string>
+
+void main()
+{
+	std::vector<std::string> name = separar();
+}
+
+std::vector<std::string> separar()
+{
+
+}
