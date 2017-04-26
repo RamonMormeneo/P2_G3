ca497cce44146634a305ee7fd60c84a9977175de
ramonmormeneoferrer <ramonmormeneoferrer@JOCS-01>
2017-04-26 11:04:39 +0200

    Lectura de archivos completada (revisar porque no lee 5 líneas)

--- Alchemy/Source.cpp
+++ Alchemy/Source.cpp
@@ -2,25 +2,33 @@
 #include <unordered_map>
 #include <fstream>
 #include <string>
-struct Key
+#include <vector>
+template<>
+struct std::hash<std::pair<std::string, std::string>>
 {
-	std::string key1;
-	std::string key2;
+	size_t operator()(const std::pair<std::string, std::string> &c) const
+	{
+
+		return((std::hash<std::string>()(c.first)
+			^ (std::hash <std::string> ()(c.second) << 1)) >> 1);
+	}
 };
 void main()
 {
-	std::unordered_map<std::pair<std::string,std::string>, std::string, Key> fusions;
+	std::unordered_map<std::pair<std::string, std::string>, std::string> fusions;
 	std::ifstream ftoread("elements.dat");
 	std::string Linea;
 	while (std::getline(ftoread, Linea))
 	{
-		Key key;
+		std::pair <std::string,std::string> key;
 		std::size_t posdeligual = Linea.find("=");
 		std::string result = Linea.substr(0, posdeligual - 1);
 		Linea = Linea.substr(posdeligual + 2);
 		std::size_t posdelmas = Linea.find("+");
-		key.key1 = Linea.substr(0, posdelmas - 1);
-		key.key2 = Linea.substr(posdelmas + 2);
-
+		key.first = Linea.substr(0, posdelmas - 1);
+		key.second = Linea.substr(posdelmas + 2);
+		fusions[key] = result;
 	}
+	std::vector <std::string> items({ "Agua","Fuego","Tierra","Aire" });
+	std::cout << items[0];
 }
